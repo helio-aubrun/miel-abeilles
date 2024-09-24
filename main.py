dc08f1c5bef7cf259f40705fa5b8df52d4f0afc0
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-09-24 15:52:26 +0200

    adding the import_flowers (), init_population () and selection ()
     import_flowers () : import the position of the flowers from an .xlsx file and put them ib a list of tuples ewample : [(1,7), (7,6)]
    init_population () : creat a list called POPULATION wich containe all the bees
    selection () : select the top bees based on distance_traveled and returne them in a list

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import openpyxl
+from bee import Bee
+
+NB_BEE = 100
+
+NB_TOP = 20
+
+POPULATION = []
+
+def import_flowers () :
+    dataframe = openpyxl.load_workbook("Champ.xlsx")
+
+    dataframe1 = dataframe.active
+
+    list_flowers = []
+
+    for row in range(1, dataframe1.max_row):
+        pos_flowers = []
+        for col in dataframe1.iter_cols(1, dataframe1.max_column):
+            pos_flowers.append ( int ((col[row].value)))
+        list_flowers.append ((pos_flowers[0],pos_flowers[1]))
+
+    return list_flowers
+
+FLOWERS = import_flowers ()
+
+def init_population () :
+    for i in range (NB_BEE) :
+        path = random.sample(FLOWERS, len(FLOWERS))
+        POPULATION.append (Bee(path))
+
+
+def selection (NB_TOP) :
+    bee_classment = sorted (POPULATION , key=lambda Bee: Bee.distance_traveled)
+    top = bee_classment [:NB_TOP]
+
+    return top
+
+
+if "__main__" == __name__ :
+    init_population ()
+    top = selection (NB_TOP)
+    for bee in top :
+        print (bee.distance_traveled)
