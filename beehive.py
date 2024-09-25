d388c7620f3d25ac41a84b9a7745cb74b0cc4b27
helio-aubrun <helio.aubrun@laplateforme.io>
2024-09-25 15:39:38 +0200

    put Bee atribute in private

--- beehive.py
+++ beehive.py
@@ -5,22 +5,22 @@ import random
 
 class Bee:
     def __init__(self, path):
-        self.path = path
-        self.compute_distance_traveled()
+        self._path = path
+        self._compute_distance_traveled()
 
-    def compute_distance_traveled(self) -> None:
+    def _compute_distance_traveled(self) -> None:
         distance_traveled = 0
         for i in range(len(self.path) - 1):
             distance_traveled += math.sqrt(
-                (self.path[i][0] - self.path[i + 1][0]) ** 2
-                + (self.path[i][1] - self.path[i + 1][1]) ** 2
+                (self._path[i][0] - self._path[i + 1][0]) ** 2
+                + (self._path[i][1] - self._path[i + 1][1]) ** 2
             )
-        self.distance_traveled = distance_traveled
+        self._distance_traveled = distance_traveled
 
     def mutation(self,mutation_frequency):
         for i in range (mutation_frequency):
-            a = random.randint(1,len(self.path)-1)
-            b = random.randint(1,len(self.path)-1)
-            tmp = self.path[b]
-            self.path[b]= self.path[a]
-            self.path[a] = tmp
+            a = random.randint(1,len(self._path)-1)
+            b = random.randint(1,len(self._path)-1)
+            tmp = self._path[b]
+            self._path[b]= self._path[a]
+            self._path[a] = tmp
