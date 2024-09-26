36d9ec1a739c5d2871295da36a7782d3a078a4cd
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-09-26 13:03:09 +0200

    adding convert_xlsx_to_txt.py and changing import_flowers()
    convert_xlsx_to_txt.py convert Champ.xlsx to Champ.txt
    and the import_flowers now import the Champ.txt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import openpyxl
 from beehive import Bee, Beehive
 
 NB_BEE = 100  # number of bees in the beehive
@@ -11,20 +10,19 @@ MUTATION_RATE = 0.2  # How many bees will mutate
 MUTATION_FREQUENCY = 2  # How many flower will change in a bee mutate path
 
 
-def import_flowers():
-    dataframe = openpyxl.load_workbook("Champ.xlsx")
-
-    dataframe1 = dataframe.active
-
-    list_flowers = []
-
-    for row in range(1, dataframe1.max_row):
-        pos_flowers = []
-        for col in dataframe1.iter_cols(1, dataframe1.max_column):
-            pos_flowers.append(int((col[row].value)))
-        list_flowers.append((pos_flowers[0], pos_flowers[1]))
-
-    return list_flowers
+def import_flowers () :
+    flowers = []
+    with open ("Champ.txt", "r") as f :
+        for line in f :
+            line = line.strip()
+            if line:
+                try :
+                    x, y = map(float, line.split(','))
+                    flowers.append((x, y))
+                except :
+                    pass
+    
+    return flowers
 
 
 FLOWERS = import_flowers()  # List of tuple that containt flowers' positions
@@ -53,5 +51,5 @@ if "__main__" == __name__:
     top = BEEHIVE.selection(SELECTION_RATE)
     BEEHIVE.multiplication_population(top, NB_BEE)
     mutate_beehive()
-    print_bees()
-    print_beehive()
+    # print_bees()
+    # print_beehive()
